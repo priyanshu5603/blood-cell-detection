12869c2617a89b93a5d57fd1eaa0a9e3dea4f40a
priyanshu5603 <113977043+priyanshu5603@users.noreply.github.com>
2025-03-29 01:02:27 +0530

    template created
    creating the springboot initialiser

---
+++ scm/src/main/java/com/scm/majProj/MajProjApplication.java
@@ -0,0 +1,13 @@
+package com.scm.majProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajProjApplication.class, args);
+	}
+
+}
---
+++ scm/src/test/java/com/scm/majProj/MajProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.majProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
